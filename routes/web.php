8c496a6bc14c12d01b69a82e3e1cbe723eed606b
AlbaRiius <arius@iesebre.com>
2021-11-16 16:38:03 +0100

    Refactoritzar la base de dades i vista tasks per utilitzar models Laravel

--- routes/web.php
+++ routes/web.php
@@ -4,30 +4,8 @@ use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $task1 = Task::create([
-        'title' => 'Tasca 1',
-        'description' => 'lorem bla bla',
-        'completed' => 0
-    ]);
-    $task2 = Task::create([
-        'title' => 'Tasca 2',
-        'description' => 'lorem bla bla',
-        'completed' => 0
-    ]);
-    $task3 = Task::create([
-        'title' => 'Tasca 3',
-        'description' => 'lorem bla bla',
-        'completed' => 1
-    ]);
-
-    $tasks = [
-        $task1,
-        $task2,
-        $task3
-    ];
-
     return view('tasks',[
-        'tasks' => $tasks
+        'tasks' => Task::all()
     ]);
 });
 
