f6a4fc5b13bdb4c6f5d2993b1e5ceebdf1cefd8e
AlbaRiius <arius@iesebre.com>
2021-11-16 15:38:57 +0100

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <div class="py-10">
+        <header>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold leading-tight text-gray-900">
+                    ABOUT
+                </h1>
+            </div>
+        </header>
+        <main>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">
+                <ul>
+                    <li>Aquest codi ha estat creat per Alba Rius Rivas. Github: <a href="https://github.com/AlbaRiius" class="underline text-blue-400">https://github.com/AlbaRiius</a> </li>
+                    <li>Codi font: <a href="https://github.com/acacha/php_for_laravel_developers" class="underline text-blue-400">https://github.com/AlbaRiius/Php_for_laravel_developers</a></li>
+                    <li>URL aplicació en explotació: <a href="http://phpforlaravel.albarius.codes" class="underline text-blue-400">http://phpforlaravel.albarius.codes</a></li>
+                </ul>
+
+            </div>
+        </main>
+    </div>
+</x-layout>
